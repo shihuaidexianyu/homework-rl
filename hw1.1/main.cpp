c7135e36bf94f11a12100a527e750b7643510ad8
shihuaidexianyu <exqinhongwei@outlook.com>
2026-09-21 16:05:32 +0800

    代码格式化

--- hw1.1/main.cpp
+++ hw1.1/main.cpp
@@ -6,57 +6,70 @@
 
 using namespace std;
 
-class TicTacToePolicyBase{
-    public:
-        virtual TicTacToe::Action operator()(const TicTacToe::State& state) const = 0;
+class TicTacToePolicyBase
+{
+public:
+    virtual TicTacToe::Action operator()(const TicTacToe::State &state) const = 0;
 };
 
 // randomly select a valid action for the step.
-class TicTacToePolicyRandom : public TicTacToePolicyBase{
-    public:
-        TicTacToe::Action operator()(const TicTacToe::State& state) const {
-            vector<TicTacToe::Action> actions = state.action_space();
-            int n_action = actions.size();
-            int action_id = rand() % n_action;
-            if (state.turn == TicTacToe::PLAYER_X){
-                return actions[action_id];
-            } else {
-                return actions[action_id];
-            }
+class TicTacToePolicyRandom : public TicTacToePolicyBase
+{
+public:
+    TicTacToe::Action operator()(const TicTacToe::State &state) const
+    {
+        vector<TicTacToe::Action> actions = state.action_space();
+        int n_action = actions.size();
+        int action_id = rand() % n_action;
+        if (state.turn == TicTacToe::PLAYER_X)
+        {
+            return actions[action_id];
         }
-        TicTacToePolicyRandom(){
-            srand(time(nullptr));
+        else
+        {
+            return actions[action_id];
         }
+    }
+    TicTacToePolicyRandom()
+    {
+        srand(time(nullptr));
+    }
 };
 
 // select the first valid action.
-class TicTacToePolicyDefault : public TicTacToePolicyBase{
-    public:
-        TicTacToe::Action operator()(const TicTacToe::State& state) const {
-            vector<TicTacToe::Action> actions = state.action_space();
-            if (state.turn == TicTacToe::PLAYER_X){
-                // TODO
-                
-                return actions[0];
-            } else {
-                return actions[0];
-            }
-        }
-        TicTacToePolicyDefault(){}
-};
+class TicTacToePolicyDefault : public TicTacToePolicyBase
+{
+public:
+    TicTacToe::Action operator()(const TicTacToe::State &state) const
+    {
+        vector<TicTacToe::Action> actions = state.action_space();
+        if (state.turn == TicTacToe::PLAYER_X)
+        {
+            // TODO
 
+            return actions[0];
+        }
+        else
+        {
+            return actions[0];
+        }
+    }
+    TicTacToePolicyDefault() {}
+};
 
 #include <chrono>
 #include <thread>
 
 // randomly select action
-int main(){
+int main()
+{
     bool done = false;
     // set verbose true
     TicTacToe env(true);
     // TicTacToePolicyDefault policy;
     TicTacToePolicyRandom policy;
-    while (not done){
+    while (not done)
+    {
         TicTacToe::State state = env.get_state();
         TicTacToe::Action action = policy(state);
         env.step(action);
